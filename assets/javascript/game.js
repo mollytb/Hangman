e4e6b488afe52a50fc3a3923ab8852ffb08bac03
Molly T Burns <Molly.t.burns@gmail.com>
2018-08-10 17:06:13 -0400

    Delete game.js

--- assets/javascript/game.js
+++
@@ -1,7 +0,0 @@
-
-//word bank for game
-var wordBank = ["corkscrew", "gatekeeper", "gemini", "magnum", "maverick", "raptor", "rougarou",
-    "valravn", "dodgem", "matterhorn", "monster", "scrambler", "skyhawk", "troika", "windseeker",
-    "maxair", "tofts", "corral", "coasters"];
-
-
